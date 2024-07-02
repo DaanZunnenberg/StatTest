5468aabd3c9c616d615444d562e5a1602f6f44cb
Daan <101355215+DaanZunnenberg@users.noreply.github.com>
2024-07-02 15:08:40 +0200

    Update (V#2)

--- Data/Reader.py
+++ Data/Reader.py
@@ -3,8 +3,11 @@ from glob import glob
 from tqdm import tqdm
 from typing import NoReturn, MutableMapping
 from pathlib import Path
+import numpy as np
+from itertools import accumulate
 import os
 from pprint import pprint
+import matplotlib.pyplot as plt
 
 __all__: list = ['Reader']
 
@@ -12,7 +15,8 @@ dir_prelim: str = str(Path(os.path.dirname(os.path.abspath(__file__))).parent)._
 
 class Reader(object):
 
-    def __init__(self, files: list = None, index_col: str = 'Unnamed: 0'):
+    def __init__(self, files: list = None, index_col: str = 'Unnamed: 0', call_dir_delim: bool = True):
+        if call_dir_delim: files = [self.dir_delim + f'\\{file}' for file in files]
         self.files = files
         self.parsed_files: MutableMapping = {}
         self.index_col = index_col
@@ -24,11 +28,19 @@ class Reader(object):
         else:
             raise TypeError('files not provided')
     
+    @property
+    def dir_delim(self) -> str: return str(Path(os.path.dirname(os.path.abspath(__file__))).parent).__add__('\\Simulations')
+
+    @staticmethod
+    def running_maximum(X): return list(accumulate(np.abs(X), max))
+
     @staticmethod
     def rename(string: str) -> str: return string.split('\\')[-1]
 
     @property
     def __exists__(self): return not isinstance(self.files, type(None))
+
+    def __repr__(self): return str(self.parsed_files)
 
 if __name__ == '__main__':
     files: list = [
@@ -50,5 +62,3 @@ if __name__ == '__main__':
        ]
     
     content = Reader(files = files)
-
-    pprint(content.parsed_files)
